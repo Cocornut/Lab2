42d2ad1eb92706fb828b68ce3a96f8f11e0b67b1
Cocornut <115100051+Cocornut@users.noreply.github.com>
2022-10-07 18:33:19 +0100

    Lab 2 1.3

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject Player;
     private Vector3 offset;
 
     void Start()
@@ -14,6 +14,6 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = Player.transform.position + offset;
     }
 }
